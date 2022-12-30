8a976aaf26d1f912fa8a934bd80f9b55e9045cb7
amakkaraka99 <amakkaraka99@gmail.com>
2022-12-29 18:27:07 -0800

    2

---
+++ src/views/layouts/Dashboard.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useAuthStore } from "../../stores/authFire";
+import { computed } from "vue";
+import Navbar from "../../components/Navbar.vue";
+
+const authStore = useAuthStore();
+const logout = () => {
+  authStore.logout();
+};
+const user = computed(() => authStore.user);
+</script>
+<template>
+  <div id="app">
+    <Navbar />
+    <p v-if="user">Welcome {{ user?.email }}</p>
+    <button @click="logout">Logout</button>
+
+    <router-view />
+  </div>
+</template>
